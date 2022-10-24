909c7df91a9a3a1bb85c653594a98f32adf5526c
Maël Chouteau <101983244+mael-chouteau@users.noreply.github.com>
2022-10-24 09:24:09 +0200

    upload: premier envoi des fichiers
    Le dossier avec les langues arrive.

---
+++ secret.exemple.py
@@ -0,0 +1,6 @@
+LOGIN="login_alcuin"
+PASS="mdp_alcuin"
+ANNEE = ["classe1","classe2","classe3"]
+CALENDRIER = ["PRJ15790","PRJ17091","PRJ16605"]
+PATHTOPDF = "/home/user/Feuille-temps/"
+COLORS = ["ORANGE Category", "RED Category", "BLUE Category"]
